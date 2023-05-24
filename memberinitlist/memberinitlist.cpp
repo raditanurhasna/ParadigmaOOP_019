a94880b6c6d9be8118623636ba7122375e0fe2a1
raditanurhasna <raditanurhasna1410@gmail.com>
2023-05-24 19:40:37 +0700

    Menampilkan class Mahasiswa secara public

--- memberinitlist/memberinitlist.cpp
+++ memberinitlist/memberinitlist.cpp
@@ -7,16 +7,16 @@ private:
 	const int id;
 	string nama;
 	float nilai;
-
 public:
 	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {
 	}
-
 	~mahasiswa() {
 		cout << "Id = " << id << endl;
 		cout << " Nama = " << nama << endl;
 		cout << "Nilai = " << nilai << endl;
 	}
 };
+
+
 
 
